5d93a38ad8e4eef5c2d2db2eec7e5e6897d74669
Mohamedbihi <simobihi72@gmail.com>
2023-04-21 10:16:48 -0700

    This file should contain your main function only.

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,10 +1,5 @@
-#ifndef calc_h
-#define calc_h
-#include <stdlib.h>
-/*
- * function_pointers.h
- * prototypes for functions
- */
+#ifndef CALC_H
+#define CALC_H
 /**
  * struct op - A struct op.
  * @op: The operator.
@@ -12,9 +7,9 @@
  */
 typedef struct op
 {
-char *op;
-int (*f)(int a, int b);
-} op_t
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
 
 int op_add(int a, int b);
 int op_sub(int a, int b);
@@ -24,3 +19,4 @@ int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 
 #endif
+
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,11 +1,12 @@
 #include "3-calc.h"
-#include <string.h>
-
+#include <stdlib.h>
 /**
- * get_op_func - chooses the right operation to perform
- * @s: operation to perform
+ * get_op_func - Selects the correct function to perform
+ *               the operation asked by the user.
+ * @s: The operator passed as argument.
  *
- * Return: pointer to the correct function
+ * Return: A pointer to the function corresponding
+ *         to the operator given as a parameter.
  */
 int (*get_op_func(char *s))(int, int)
 {
@@ -15,16 +16,14 @@ int (*get_op_func(char *s))(int, int)
 		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
-		{NULL, NULL}
+		{NULL, NULL},
 	};
-	int i;
 
-	i = 0;
-	while (ops[i].op != NULL)
-	{
-		if (strcmp(s, ops[i].op) == 0)
-			break;
+	int i = 0;
+
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
-	}
+
 	return (ops[i].f);
 }
+
